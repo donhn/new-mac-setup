8f2aac4b43a0d03fe1f114ef96ea82af5502feea
Don Nguyen <don@Dons-MacBook-Pro.local>
2022-09-15 16:31:30 -0700

    fix: cd into repo after mkdir

--- new_mac.sh
+++ new_mac.sh
@@ -13,7 +13,7 @@ brew install python iterm2 neovim tmux fish gh lazygit ripgrep fd
 open -a terminal.app ./configure_fish.sh
 
 # Download dotfiles and configure nvim.
-mkdir ~/repo && ~/repo
+mkdir ~/repo && cd ~/repo
 git clone https://github.com/donhn/dotfiles.git 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 cp -r ~/repo/dotfiles/.config/. ~/.config
